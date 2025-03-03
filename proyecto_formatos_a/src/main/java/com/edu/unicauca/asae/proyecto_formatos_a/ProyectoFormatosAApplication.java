6ea6a105e691a91c2a9633a73571bea6910c7995
rscruz <santiagocrzzz1711@gmail.com>
2025-03-03 15:30:58 -0500

    Adding spring initializer

---
+++ proyecto_formatos_a/src/main/java/com/edu/unicauca/asae/proyecto_formatos_a/ProyectoFormatosAApplication.java
@@ -0,0 +1,13 @@
+package com.edu.unicauca.asae.proyecto_formatos_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFormatosAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFormatosAApplication.class, args);
+	}
+
+}
---
+++ proyecto_formatos_a/src/test/java/com/edu/unicauca/asae/proyecto_formatos_a/ProyectoFormatosAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.unicauca.asae.proyecto_formatos_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFormatosAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
