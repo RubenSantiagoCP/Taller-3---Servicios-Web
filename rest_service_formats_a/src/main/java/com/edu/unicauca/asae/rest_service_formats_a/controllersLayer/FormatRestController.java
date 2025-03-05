8da73496d35f2d47903ea29d339aae6fdddf8fe1
rscruz <santiagocrzzz1711@gmail.com>
2025-03-04 23:15:54 -0500

    a

--- rest_service_formats_a/src/main/java/com/edu/unicauca/asae/rest_service_formats_a/controllersLayer/FormatRestController.java
+++ rest_service_formats_a/src/main/java/com/edu/unicauca/asae/rest_service_formats_a/controllersLayer/FormatRestController.java
@@ -1,5 +1,8 @@
 package com.edu.unicauca.asae.rest_service_formats_a.controllersLayer;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/api/format")
 public class FormatRestController {
